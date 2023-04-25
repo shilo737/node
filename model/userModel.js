ccff93c7fc8fec6ae31f4917eb3e9e5416886eb7
shilo fun <shilof7979@gmail.com>
2023-04-25 13:56:16 +0300

    users

---
+++ model/userModel.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose")
+const Joi =  require("joi")
+
+const userSchema = new mongoose.Schema({
+    name:String,
+    email:String,
+    password:String
+},{timestamps:true})
+
+exports.UserModel = mongoose.model("users",userSchema)
+
+exports.validataUser = (_bodyData) =>{
+    const joiSchema = Joi.object({
+    name:Joi.string().min(2).max(30).required(),       
+    email:Joi.string().min(2).max(50).email().required(),       
+    password:Joi.string().min(3).max(200).required()        
+    })
+    return joiSchema.validate(_bodyData)
+    }
--- routes/configRouters.js
+++ routes/configRouters.js
@@ -1,8 +1,9 @@
 const indexR = require("./index")
 const songsR = require("./songs")
-
+const usersR = require("./users")
 
 exports.routesInit = (app) => {
 app.use("/",indexR)
 app.use("/songs",songsR)
+app.use("/users",usersR)
 }
