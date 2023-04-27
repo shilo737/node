852bd473971261dfe2ca04c5298139935286eb37
shilo fun <shilof7979@gmail.com>
2023-04-27 12:38:54 +0300

    users

--- model/userModel.js
+++ model/userModel.js
@@ -1,19 +1,30 @@
-const mongoose = require("mongoose")
-const Joi =  require("joi")
+const mongoose = require("mongoose");
+const Joi = require("joi");
 
-const userSchema = new mongoose.Schema({
-    name:String,
-    email:String,
-    password:String
-},{timestamps:true})
+const userSchema = new mongoose.Schema(
+  {
+    name: String,
+    email: String,
+    password: String,
+  },
+  { timestamps: true }
+);
 
-exports.UserModel = mongoose.model("users",userSchema)
+exports.UserModel = mongoose.model("users", userSchema);
 
-exports.validataUser = (_bodyData) =>{
-    const joiSchema = Joi.object({
-    name:Joi.string().min(2).max(30).required(),       
-    email:Joi.string().min(2).max(50).email().required(),       
-    password:Joi.string().min(3).max(200).required()        
-    })
-    return joiSchema.validate(_bodyData)
-    }
+exports.validataUser = (_bodyData) => {
+  const joiSchema = Joi.object({
+    name: Joi.string().min(2).max(30).required(),
+    email: Joi.string().min(2).max(50).email().required(),
+    password: Joi.string().min(3).max(200).required(),
+  });
+  return joiSchema.validate(_bodyData);
+};
+
+exports.validataLogin = (_bodyData) => {
+  const joiSchema = Joi.object({
+    email: Joi.string().min(2).max(50).email().required(),
+    password: Joi.string().min(3).max(200).required(),
+  });
+  return joiSchema.validate(_bodyData);
+};
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,50 @@
 const express = require("express");
-const router = express.Router()
+const bcrypt = require("bcrypt")
+const router = express.Router();
+const { UserModel, validataUser, validataLogin } = require("../model/userModel");
 
-router.get("/",async(req,res)=>{
-    res.json({msg:"user work"})
+
+router.get("/", async (req, res) => {
+  const data = await UserModel.find({});
+  res.json(data);
+});
+
+router.post("/", async (req, res) => {
+  const validBody = validataUser(req.body);
+  if (validBody.error) {
+    return res.status(401).json(validBody.error.details);
+  }
+
+  try {
+    const user = new UserModel(req.body)
+    user.password = await bcrypt.hash(user.password,10)
+    await user.save()
+    user.password = "*****"
+    res.json(user)
+  } catch (err) {
+    if(err.code == 11000){
+        return res.status(401).json({err:"Email already in system",code:11000})
+    }
+    console.log(err);
+    res.status(502).json({err});
+  }
+});
+
+router.post("/login",async(req,res) => {
+    const validBody = validataLogin(req.body);
+    if (validBody.error) {
+      return res.status(401).json(validBody.error.details);
+    }
+    try{
+
+        
+        
+    }
+    catch(err){
+        console.log(err);
+        res.status(502).json({err})
+    }
 
 })
 
-module.exports = router
+module.exports = router;
